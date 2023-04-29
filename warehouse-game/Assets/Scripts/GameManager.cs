a8de140688f72a937d21d39462a21bb3efe80d6f
Bar Goldenstein <bargolden12@gmail.com>
2023-04-29 12:18:05 +0300

    added randomly spawning boxes (ong!!!!)

---
+++ warehouse-game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public Vector2[] boxPositions;
+
+    private void Start() => InvokeRepeating("SpawnBox", 0, 0.1f);
+
+    private void SpawnBox() {
+        if (Random.value >= 0.1f) return;
+        Instantiate(boxPrefab, boxPositions[Random.Range(0, 6)], Quaternion.identity);
+    }
+}
