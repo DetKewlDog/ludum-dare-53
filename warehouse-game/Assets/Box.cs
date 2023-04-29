f12738f0602ecf0adecdfb450bc9b1e393307453
Bar Goldenstein <bargolden12@gmail.com>
2023-04-29 17:25:22 +0300

    ADDED BETTER DRAGGING (VERY FUN!!!)

--- warehouse-game/Assets/Box.cs
+++ warehouse-game/Assets/Box.cs
@@ -14,9 +14,10 @@ public class Box : MonoBehaviour
     [Space]
     public BoxColor boxColor;
     Camera cam;
-    Vector3 mousePosition = Vector3.zero;
     Vector3 offset = new Vector3(0, 0, 10);
     Rigidbody2D rb;
+    Vector3 force = Vector3.zero, mousePos;
+    LineRenderer line;
 
     void Awake() {
         boxColor = (BoxColor)Random.Range(0, 3);
@@ -25,9 +26,19 @@ public class Box : MonoBehaviour
         GetComponent<SpriteRenderer>().sprite = sprites[index];
         GetComponent<BoxCollider2D>().size = sizes[index];
         rb = GetComponent<Rigidbody2D>();
+        line = GetComponent<LineRenderer>();
     }
 
     void Start() => cam = Camera.main;
 
-    private void OnMouseDrag() => rb.MovePosition(cam.ScreenToWorldPoint(Input.mousePosition) + offset);
+    private void OnMouseDrag() {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+        rb.AddForce((mousePos - transform.position) * 5 + Vector3.down * 5);
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, mousePos);
+    }
+
+    private void OnMouseDown() => line.positionCount = 2;
+
+    private void OnMouseUp() => line.positionCount = 0;
 }
---
+++ warehouse-game/Assets/CursorHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CursorHandler : MonoBehaviour
+{
+    Camera cam;
+    Vector3 offset = new Vector3(0, 0, 10);
+    void Start() {
+        Cursor.visible = false;
+        cam = Camera.main;
+    }
+    void Update() => transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + offset;
+}
