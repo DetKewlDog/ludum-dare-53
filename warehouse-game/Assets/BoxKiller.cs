f2e12f94b11cc317b09ddcd216cc97eb15cf5103
Bar Goldenstein <bargolden12@gmail.com>
2023-04-29 12:25:35 +0300

    added box destruction

---
+++ warehouse-game/Assets/BoxKiller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BoxKiller : MonoBehaviour
+{
+    private GameManager gameManager;
+    void Start() {
+        gameManager = GameManager.instance;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (!other.CompareTag("Box")) return;
+        gameManager.BoxDestroyed();
+        Destroy(other.gameObject);
+    }
+}
--- warehouse-game/Assets/Scripts/GameManager.cs
+++ warehouse-game/Assets/Scripts/GameManager.cs
@@ -2,13 +2,20 @@
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     public GameObject boxPrefab;
     public Vector2[] boxPositions;
 
-    private void Start() => InvokeRepeating("SpawnBox", 0, 0.1f);
+    private void Awake() {
+        instance = this;
+        InvokeRepeating("SpawnBox", 0, 0.1f);
+    }
 
     private void SpawnBox() {
         if (Random.value >= 0.1f) return;
         Instantiate(boxPrefab, boxPositions[Random.Range(0, 6)], Quaternion.identity);
     }
+
+    public void BoxDestroyed() => print("L BOZO YOUR BOX FELL OFF");
 }
