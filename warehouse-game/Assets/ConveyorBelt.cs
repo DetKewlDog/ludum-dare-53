d3b8f5b008161c913df67c17d75517525599a63a
Bar Goldenstein <bargolden12@gmail.com>
2023-04-29 11:57:17 +0300

    CONVEYOR BELTS

---
+++ warehouse-game/Assets/ConveyorBelt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float Speed = 1;
+    public bool IsActive = true;
+
+    private Rigidbody2D rb;
+    private Vector2 pos, originalPos;
+
+    private void Start() => rb = GetComponent<Rigidbody2D>();
+
+    // Update is called once per frame
+    private void FixedUpdate() {
+        if (!IsActive) return;
+        originalPos = pos = rb.position;
+        pos.x += Speed * Time.deltaTime;
+        rb.position = pos;
+        rb.MovePosition(originalPos);
+    }
+}
