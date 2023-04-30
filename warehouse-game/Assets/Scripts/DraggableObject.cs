93fc7f7db7d99befa720251049f5ce6c26dab886
Bar Goldenstein <bargolden12@gmail.com>
2023-04-30 12:32:07 +0300

    removed unused line renderers

--- warehouse-game/Assets/Scripts/DraggableObject.cs
+++ warehouse-game/Assets/Scripts/DraggableObject.cs
@@ -4,20 +4,15 @@ using UnityEngine.EventSystems;
 public class DraggableObject : MonoBehaviour
 {
     protected Camera cam;
-    protected Vector3 offset = new Vector3(0, 0, 10);
     protected Rigidbody2D rb;
-    protected Vector3 force = Vector3.zero, mousePos, originalMousePos, originalPos;
-    protected LineRenderer line;
+    protected Vector3 mousePos, originalMousePos, originalPos;
 
-    protected virtual void Awake() {
-        rb = GetComponent<Rigidbody2D>();
-        line = GetComponent<LineRenderer>();
-    }
+    protected virtual void Awake() => rb = GetComponent<Rigidbody2D>();
 
     protected void Start() => cam = Camera.main;
     protected  void OnMouseDrag() {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - originalMousePos;
-        rb.velocity = (originalPos + mousePos - rb.transform.position) * 500 * Time.deltaTime;
+        rb.velocity = (originalPos + mousePos - rb.transform.position) * 300 * Time.deltaTime;
     }
 
     private void OnMouseDown() {
