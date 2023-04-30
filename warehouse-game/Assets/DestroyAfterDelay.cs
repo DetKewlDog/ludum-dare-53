ed6203a02a1faf5e66145b1bd55fce517266668e
Bar Goldenstein <bargolden12@gmail.com>
2023-04-30 16:10:27 +0300

    added box particle

---
+++ warehouse-game/Assets/DestroyAfterDelay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    void Start() => StartCoroutine(Co());
+
+    IEnumerator Co() {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
